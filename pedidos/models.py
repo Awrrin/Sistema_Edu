372b9b41cac599f87e7a26c57fc60ab708b0a3d7
Awrrin <clebersonezb@gmail.com>
2024-04-28 21:01:00 -0300

    alterração de modelos 002

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
 
-# Create your models here.
-# class Category(models.Model):
-# name = models.CharField(max_length=100, null=False, blank=False)
-
-# class Product(models.Model):
-# product = models.CharField(max_length=400, null=False, blank=False)
-# descrition = models.CharField(max_length=200, null=False, blank=False)
-# category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+class Product(models.Model):
+    product = models.CharField(max_length=400, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Pedidos(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
-    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(null=True)
     obser = models.CharField(max_length=300, null=True)
     finished_at = models.DateTimeField(null=True)
 
-    # Outros campos do modelo...
